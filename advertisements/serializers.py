15b49e038335efa42bc67ddcae7dce87282f434b
Romann37 <mihalkovo62@gmail.com>
2022-02-10 20:26:23 +0300

    first commit

---
+++ advertisements/serializers.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.serializers import ValidationError
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        adv_open = Advertisement.objects.filter(creator=self.context["request"].user, status='OPEN').count()
+        if adv_open >= 10:
+            raise ValidationError('Превышено допустимое количество открытых объявлений!')
+
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+
+        adv = Advertisement.objects.filter(creator=self.context["request"].user, status='OPEN').count()
+        if data.get('status', '') == 'OPEN':
+            if adv >= 10:
+                raise ValidationError('Превышено допустимое количество открытых объявлений!')
+
+        return data
